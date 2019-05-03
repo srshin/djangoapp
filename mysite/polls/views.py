cfe565e8729fd5957cab78af9169e4cb6e3b419c
srshin <sangrimshin@gmail.com>
2019-05-03 15:25:53 +0900

    django : use template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
 # Create your views here.
 
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {'text': 'Hello, world. You\'re at the polls index'}
+    return render(request, 'polls/index.html', context)
